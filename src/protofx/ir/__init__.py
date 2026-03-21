0a5dae018a5fd06ab4a6cc295d7a139aa2bbf0e2
kwjinwoo <jung003327@gmail.com>
2026-03-22 00:43:41 +0900

    feat(ir): export Value and ValueKind from ir package

--- src/protofx/ir/__init__.py
+++ src/protofx/ir/__init__.py
@@ -4,5 +4,6 @@ from protofx.ir.dim import Dim
 from protofx.ir.dtype import DType
 from protofx.ir.shape import Shape
 from protofx.ir.tensor_type import TensorType
+from protofx.ir.value import Value, ValueKind
 
-__all__ = ["DType", "Dim", "Shape", "TensorType"]
+__all__ = ["DType", "Dim", "Shape", "TensorType", "Value", "ValueKind"]
