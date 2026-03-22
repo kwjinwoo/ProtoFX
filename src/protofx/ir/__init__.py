8a68ed9f58f8cc488b24ebc84676dc78d92177f4
kwjinwoo <jung003327@gmail.com>
2026-03-22 11:12:46 +0900

    feat(ir): export graph from ir package __init__

--- src/protofx/ir/__init__.py
+++ src/protofx/ir/__init__.py
@@ -2,9 +2,10 @@
 
 from protofx.ir.dim import Dim
 from protofx.ir.dtype import DType
+from protofx.ir.graph import Graph
 from protofx.ir.node import AttributeValue, Node
 from protofx.ir.shape import Shape
 from protofx.ir.tensor_type import TensorType
 from protofx.ir.value import Value, ValueKind
 
-__all__ = ["AttributeValue", "DType", "Dim", "Node", "Shape", "TensorType", "Value", "ValueKind"]
+__all__ = ["AttributeValue", "DType", "Dim", "Graph", "Node", "Shape", "TensorType", "Value", "ValueKind"]
